b588d6f88f563c87b11cd497237549a47f88c044
Ryan Christian Noble <154901707+RyanCodeCraft13@users.noreply.github.com>
2026-02-09 21:26:12 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,22 +1,53 @@
-import { auth } from "./firebase.js";
-import { 
+import { auth, db } from "./firebase.js";
+
+import {
 createUserWithEmailAndPassword,
 signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-window.signup = async function(){
-let email=document.getElementById("email").value;
-let pass=document.getElementById("pass").value;
+import {
+doc,setDoc,getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-await createUserWithEmailAndPassword(auth,email,pass);
+window.signup = async function(){
+
+let email=emailInput.value;
+let pass=passInput.value;
+let role=roleInput.value;
+let pin=pinInput.value;
+
+const userCred=await createUserWithEmailAndPassword(auth,email,pass);
+
+await setDoc(doc(db,"users",userCred.user.uid),{
+email,role,pin
+});
+
 alert("Account Created");
 window.location="login.html";
 }
 
-window.login = async function(){
-let email=document.getElementById("email").value;
-let pass=document.getElementById("pass").value;
+window.loginEmail = async function(){
+
+let email=emailInput.value;
+let pass=passInput.value;
 
 await signInWithEmailAndPassword(auth,email,pass);
+
+localStorage.setItem("login","true");
+
 window.location="pos.html";
+}
+
+window.loginPIN = async function(){
+
+let pin=pinInput.value;
+
+const snapshot=await getDoc(doc(db,"pins",pin));
+
+if(snapshot.exists()){
+localStorage.setItem("login","true");
+window.location="pos.html";
+}else{
+alert("Invalid PIN");
+}
 }
