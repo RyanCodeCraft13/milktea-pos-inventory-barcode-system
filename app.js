9151e643192e2dce7cbd2f7d0ad854b57bc44361
Ryan Christian Noble <154901707+RyanCodeCraft13@users.noreply.github.com>
2026-02-09 16:04:49 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const products = {
+ "MT001": {name:"Wintermelon", price:120},
+ "MT002": {name:"Okinawa", price:110},
+ "MT003": {name:"Taro", price:130}
+};
+
+let total=0;
+
+function scanProduct(){
+ let code=document.getElementById("barcode").value;
+ let product=products[code];
+
+ if(product){
+  total+=product.price;
+
+  let li=document.createElement("li");
+  li.innerText=product.name+" ₱"+product.price;
+  document.getElementById("cart").appendChild(li);
+
+  document.getElementById("total").innerText=total;
+ }
+}
